6236fd34d913eb3b2c538c4e3113831a580fdbec
ETTARAK <ettarak.zouhair@gmail.com>
2021-01-14 16:26:03 +0100

    Coding, Add Package Primeng@8.00 And primeicons@1.00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import {ButtonModule} from 'primeng/button';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {FieldsetModule} from 'primeng/fieldset';
+import {TabViewModule} from 'primeng/tabview';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+
+
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +20,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    SplitButtonModule,
+    FieldsetModule,
+    TabViewModule,
+    TabMenuModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
